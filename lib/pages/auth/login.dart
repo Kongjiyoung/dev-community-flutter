90df0d8cd88d3b2ed265505410b2111b19ac14d4
kjs <zeeq125@gmail.com>
2024-07-09 12:45:30 +0900

    http, move 추가

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -7,48 +7,48 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("로그인"),
+        title: const Text("로그인"),
       ),
       body: Container(
-        padding: EdgeInsets.fromLTRB(25, 10, 25, 25),
+        padding: const EdgeInsets.fromLTRB(25, 10, 25, 25),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "이메일",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: "example@publy.com",
+              ),
+            ),
+            Text("비밀번호"),
+            TextFormField(
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: "영문, 숫자 포함 6자 이상",
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("이메일로 계속하기")),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("이메일", style:
-                TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                    hintText: "example@publy.com",
-                  ),
-                ),
-                Text("비밀번호"),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                    hintText: "영문, 숫자 포함 6자 이상",
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(onPressed: () {
-
-                  },
-                      child: Text("이메일로 계속하기")
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("고객센터"),
-                    Text("비밀번호를 잊으셨나요?"),
-                  ],
-                ),
+                Text("고객센터"),
+                Text("비밀번호를 잊으셨나요?"),
               ],
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/auth/welcome_page.dart
+++ lib/pages/auth/welcome_page.dart
@@ -1,6 +1,9 @@
+import 'package:dev_community/_core/constants/move.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,10 +11,15 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           children: [
             Image.asset("/images/Title.png"),
-            Text("요즘 개발자들의 필수 \n커뮤니티"),
-            Text("개발 트렌드부터 Q&A, 네트워킹까지\n꼭 필요한 정보를 한 번에"),
+            const Text("요즘 개발자들의 필수 \n커뮤니티"),
+            const Text("개발 트렌드부터 Q&A, 네트워킹까지\n꼭 필요한 정보를 한 번에"),
             Image.asset("/images/kakao_button.png"),
-            Text("다른 방법으로 시작하기"),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, Move.loginPage);
+              },
+              child: const Text("다른 방법으로 시작하기"),
+            ),
           ],
         ),
       ),
