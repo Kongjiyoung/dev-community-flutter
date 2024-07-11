d64dd2f587b54f8025d88960c2f30f566f12bf58
kjs <zeeq125@gmail.com>
2024-07-11 15:32:46 +0900

    마이페이지 수정

--- lib/_core/constants/move.dart
+++ lib/_core/constants/move.dart
@@ -2,6 +2,7 @@ import 'package:dev_community/pages/auth/join.dart';
 import 'package:dev_community/pages/auth/login.dart';
 import 'package:dev_community/pages/auth/welcome_page.dart';
 import 'package:dev_community/pages/home/home_page.dart';
+import 'package:dev_community/pages/main_page.dart';
 import 'package:dev_community/pages/my/my_page.dart';
 import 'package:dev_community/pages/qna/qna_page.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,7 @@ class Move {
   static String welcomePage = "/welcome";
   static String loginPage = "/login";
   static String joinPage = "/join";
+  static String mainPage = "/main";
   static String homePage = "/home";
   static String myPage = "/my";
   static String qnaPage = "/qna";
@@ -20,6 +22,7 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.welcomePage: (context) => const WelcomePage(),
     Move.loginPage: (context) => const LoginPage(),
     Move.joinPage: (context) => const JoinPage(),
+    Move.mainPage: (context) => const MainPage(),
     Move.homePage: (context) => const HomePage(),
     Move.myPage: (context) => const MyPage(),
     Move.qnaPage: (context) => const QAndAPage(),
