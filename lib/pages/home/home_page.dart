32536c40307b21c2cf52e3efe5936df9c4ab9c47
jiyoung <ij0512@naver.com>
2024-07-12 16:34:04 +0900

    리스트빌더완료

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -11,6 +11,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final posts = [
+      {
+        "name": "공지영",
+        "position": "신입개발자",
+        "time": "3시간 전",
+        "title": "제목 : FlashAttention3 공개",
+        "content": "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
+        "views": "조회 698",
+      },
+      {
+        "name": "공지영",
+        "position": "신입개발자",
+        "time": "3시간 전",
+        "title": "제목 : FlashAttention3 공개",
+        "content": "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
+        "views": "조회 698",
+      },
+      {
+        "name": "공지영",
+        "position": "신입개발자",
+        "time": "3시간 전",
+        "title": "제목 : FlashAttention3 공개",
+        "content": "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
+        "views": "조회 698",
+      },
+      {
+        "name": "공지영",
+        "position": "신입개발자",
+        "time": "3시간 전",
+        "title": "제목 : FlashAttention3 공개",
+        "content": "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
+        "views": "조회 698",
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -42,96 +77,79 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Row(
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    // backgroundColor: Color(0xffd3ffba), // 배경색 설정
-                    side: BorderSide(color: Color(0xff727a72), width: 1.0),
-                    // 테두리 설정
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0), // 둥근 모서리 설정
+      body: ListView.builder(
+        itemCount: posts.length + 1, // 추가로 버튼들을 위해 +1
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            // 첫 번째 항목으로 버튼들을 추가
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      // backgroundColor: Color(0xffd3ffba), // 배경색 설정
+                      side: BorderSide(color: Color(0xff727a72), width: 1.0),
+                      // 테두리 설정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0), // 둥근 모서리 설정
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.whatshot,
+                          color: Color(0xfffa8783),
+                        ),
+                        const Text(
+                          "인기글",
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.whatshot,
-                        color: Color(0xfffa8783),
+                  SizedBox(width: 10),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      side: BorderSide(color: Color(0xff727a72), width: 1.0),
+                      // 테두리 설정
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0), // 둥근 모서리 설정
                       ),
-                      const Text(
-                        "인기글",
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 10),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    // backgroundColor: Color(0xffd3ffba), // 배경색 설정
-                    side: BorderSide(color: Color(0xff727a72), width: 1.0),
-                    // 테두리 설정
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0), // 둥근 모서리 설정
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.bookmark,
+                          color: Color(0xfffa8783),
+                        ),
+                        const Text(
+                          "저장한 게시물",
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.bookmark,
-                        color: Color(0xfffa8783),
-                      ),
-                      const Text(
-                        "저장한 게시물",
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          BuildPost(
-            name: "공지영",
-            job: "신입개발자",
-            time: "3시간 전",
-            title: "제목 : FlashAttention3 공개",
-            content: "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
-            views: "조회 698",
-          ),
-          SizedBox(height: 10),
-          BuildPost(
-            name: "공지영",
-            job: "신입개발자",
-            time: "3시간 전",
-            title: "제목 : FlashAttention3 공개",
-            content: "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
-            views: "조회 698",
-          ),
-          SizedBox(height: 10),
-          BuildPost(
-            name: "공지영",
-            job: "신입개발자",
-            time: "3시간 전",
-            title: "제목 : FlashAttention3 공개",
-            content: "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
-            views: "조회 698",
-          ),
-          SizedBox(height: 10),
-          BuildPost(
-            name: "공지영",
-            job: "신입개발자",
-            time: "3시간 전",
-            title: "제목 : FlashAttention3 공개",
-            content: "내용 : LLM의 속도 향상을 위해서 최근에 많은 방법들이 시도되고 있는데요,.. ",
-            views: "조회 698",
-          ),
-        ],
+                ],
+              ),
+            );
+          }
+          // 버튼들을 제외한 나머지 항목으로 게시물들 추가
+          final post = posts[index - 1]; // 첫 번째 항목이 버튼이므로 인덱스를 조정
+          return Column(
+            children: [
+              BuildPost(
+                name: post["name"]!,
+                job: post["position"]!,
+                time: post["time"]!,
+                title: post["title"]!,
+                content: post["content"]!,
+                views: post["views"]!,
+              ),
+              SizedBox(height: 10),
+            ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
