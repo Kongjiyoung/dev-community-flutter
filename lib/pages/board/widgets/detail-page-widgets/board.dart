596536f7190039acbc4f619129ae8282ed623d56
egdg4587 <egdg7777@gmail.com>
2024-07-18 18:02:56 +0900

    게시글 상세보기

--- lib/pages/board/widgets/detail-page-widgets/board.dart
+++ lib/pages/board/widgets/detail-page-widgets/board.dart
@@ -3,20 +3,28 @@ import 'package:dev_community/pages/board/widgets/detail-page-widgets/title.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
 
+import '../../../../dtos/board/board_response.dart';
 import 'content.dart';
 import 'like-manager.dart';
 
 class Board extends StatelessWidget {
-  const Board({
-    super.key,
-    required this.title,
-    required quill.QuillController quillController,
-    required this.likeManager,
-  }) : _quillController = quillController;
-
   final String title;
   final quill.QuillController _quillController;
   final LikeManager likeManager;
+  final int boardHit;
+  final String createdAt;
+  final int replyCount;
+  final List<ReplyDTO> replies;
+
+  const Board({
+    required this.title,
+    required quill.QuillController quillController,
+    required this.likeManager,
+    required this.createdAt,
+    required this.replyCount,
+    required this.boardHit,
+    required this.replies,
+  }) : _quillController = quillController;
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +36,14 @@ class Board extends StatelessWidget {
           children: [
             SizedBox(height: 10),
             MyTitle(title: title),
-            Content(quillController: _quillController, likeManager: likeManager),
-            ReplyAll(),
+            Content2(
+                quillController: _quillController, likeManager: likeManager),
+            ReplyAll(
+              replies: replies,
+              boardHit: boardHit,
+              createdAt: createdAt,
+              replyCount: replyCount,
+            ),
           ],
         ),
       ),
